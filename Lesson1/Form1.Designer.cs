ba510f0c4abf963b4ea6d422d0dc7afdbb4de6f0
Irina Khmyrova <khmyrova.irishka@mail.ru>
2020-09-26 10:15:22 +0300

    Добавьте файлы проекта.

---
+++ Lesson1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Lesson1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button.Location = new System.Drawing.Point(211, 46);
+            this.button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(227, 64);
+            this.button.TabIndex = 0;
+            this.button.Text = "Нажми на меня";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.UseWaitCursor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.SystemColors.Control;
+            this.label.Font = new System.Drawing.Font("Haettenschweiler", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label.Location = new System.Drawing.Point(261, 156);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(120, 40);
+            this.label.TabIndex = 1;
+            this.label.Text = "Спасибо!";
+            this.label.Visible = false;
+            this.label.Click += new System.EventHandler(this.label_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(727, 428);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Нажми на кнопку";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label;
+    }
+}
+
